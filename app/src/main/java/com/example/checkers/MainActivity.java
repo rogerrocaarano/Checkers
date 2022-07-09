0086659fcbdaf757ef60cdc487db38176b3cda7a
Róger Roca Arano <rogerroca@reinseg.com>
2022-07-08 22:19:40 -0400

    Changed ids of board buttons.

--- app/src/main/java/com/example/checkers/MainActivity.java
+++ app/src/main/java/com/example/checkers/MainActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private CheckersGame Game;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StartGame();
+    }
+    public void StartGame() {
+        Game=new CheckersGame();
     }
 }
